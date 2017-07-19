c441954b0ce9e954c408cc2a6009380e0f6b4239
aftaberski <aftaberski@gmail.com>
2017-07-19 17:10:27 -0400

    Add method to add block to BlockChain

--- ktcoin_test.go
+++ ktcoin_test.go
@@ -64,3 +64,17 @@ func TestVerify(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestAddBlock(t *testing.T) {
+	bc := NewBlockChain()
+	transactions := make([]Transaction, 0)
+	transactionBytes := make([]byte, 0)
+	transactionBytes = append(transactionBytes, []byte("test")...)
+	recipient, _ := rsa.GenerateKey(rand.Reader, 2048)
+	signature := make([]byte, 0)
+	transactions = append(transactions, Transaction{transactionBytes, &recipient.PublicKey, signature})
+	bc.addNextBlock(transactions)
+	if len(bc.blocks) != 2 {
+		t.Fail()
+	}
+}
